1b13583cef92f34c05cd07b83e767d1f9cf2a3cf
visheraaaa <simpliest1email@gmail.com>
2025-09-03 21:42:04 +0300

    Added get/set methods to model

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -2,10 +2,10 @@
 {
     public class TaskItem
     {
-        public int Id;
-        public string Title;
-        public string Description;
-        public bool IsCompleted;
-        public DateTime CreatedAt;
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
