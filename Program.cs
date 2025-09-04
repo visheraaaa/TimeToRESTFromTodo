f6bc0bd34a2f47ab43abb635f0472f2d7294c476
visheraaaa <simpliest1email@gmail.com>
2025-09-04 23:18:26 +0300

    Added delete-method

--- Program.cs
+++ Program.cs
@@ -11,14 +11,12 @@ var app = builder.Build();
 var tasks = new List<TaskItem>();
 
 
-
 app.MapGet("/tasks", () => tasks);
 
-app.MapGet("/tasks/{id}", (Guid id) =>
+app.MapGet("/task/{id}", (Guid id) =>
     tasks.FirstOrDefault(t => t.Id == id) is TaskItem task 
     ? Results.Ok(task) 
     : Results.NotFound());
-
 
 app.MapPost("/tasks", (TaskCreateDTO TaskRequest) =>
 {
@@ -26,7 +24,6 @@ app.MapPost("/tasks", (TaskCreateDTO TaskRequest) =>
     tasks.Add(NewTask);
     return Results.Created($"/tasks/{NewTask.Id}", NewTask);
 });
-
 
 app.MapPatch("/task/{id}", (Guid id, TaskPatchDTO PatchDTO) =>
 {
@@ -45,29 +42,24 @@ app.MapPatch("/task/{id}", (Guid id, TaskPatchDTO PatchDTO) =>
     return Results.Ok(task);
 });
 
+app.MapDelete("/task/{id}", (Guid id) =>
+{
+    foreach(TaskItem task in tasks)
+    {
+        if (task.Id == id) tasks.Remove(task);
+        return Results.Ok();
 
+    }
+    return Results.NotFound();
+});
 
 app.MapGet("/", () => Results.Redirect("/swagger"));
+
+
+
 app.UseRouting();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
-
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseAuthorization();
-
-//app.MapStaticAssets();
-//app.MapRazorPages()
-//   .WithStaticAssets();
 
 app.Run();
